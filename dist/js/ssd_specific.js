6e49a35641f3693b481eae39c07e60467f53f68e
Merrick28 <stephane.dewitte@gmail.com>
2020-11-10 11:18:49 +0100

    bouton générique

--- dist/js/ssd_specific.js
+++ dist/js/ssd_specific.js
@@ -27,52 +27,54 @@ $(document).ready(function () {
 
 
     // on va maintenant intercepter le click sur le bouton status-radarr
-    $("#status-radarr").click(function () {
+    $(".bouton-install").click(function () {
+        let appli = $(this).attr("data-appli");
+        console.log("Appli appelée " + appli)
         // on va considérer que le texte du bouton est ok
         // a voir si on refait un appel ajax pour vérifier ?
-        if ($("#status-radarr").html() === "Installer") {
-            $("#status-radarr").html("Installation...").prop('disabled', true);
+        if ($("#status-" + appli).html() === "Installer") {
+            $("#status-" + appli).html("Installation...").prop('disabled', true);
             // on lance un ajax qui va installer tout ça
             $.ajax({
-                url: "http://178.170.54.173/ajax/install_service.php?service=radarr"
+                url: "http://178.170.54.173/ajax/install_service.php?service=" + appli
             }).done(function (data) {
                 // On est dans le done, tout est ok
                 // la requête est passée
                 // le résultat de la requête est maintenant dans la variable "data"
                 if (data === "ok") {
                     // le service a été installé
-                    $("#status-radarr").html("Désinstaller").prop('disabled', false);
-                    $(".start-stop-button-radarr").show();
+                    $("#status-" + appli).html("Désinstaller").prop('disabled', false);
+                    $(".start-stop-button-" + appli).show();
                 } else {
                     // il y a eu une erreur sur l'installation
-                    $("#status-radarr").html("Erreur sur installation");
-                    $(".start-stop-button-radarr").hide();
+                    $("#status-" + appli).html("Erreur sur installation");
+                    $(".start-stop-button-" + appli).hide();
                 }
             }).fail(function () {
                 console.log('Erreur sur le chargement de l\'ajax, impossible de continuer');
-                $("#status-radarr").html("Erreur ajax");
+                $("#status-" + appli).html("Erreur ajax");
             });
 
-        } else if ($("#status-radarr").html() === "Désinstaller") {
-            $("#status-radarr").html("Désinstallation...").prop('disabled', true);
+        } else if ($("#status-" + appli).html() === "Désinstaller") {
+            $("#status-" + appli).html("Désinstallation...").prop('disabled', true);
             $.ajax({
-                url: "http://178.170.54.173/ajax/install_service.php?service=radarr"
+                url: "http://178.170.54.173/ajax/install_service.php?service=" + appli
             }).done(function (data) {
                 // On est dans le done, tout est ok
                 // la requête est passée
                 // le résultat de la requête est maintenant dans la variable "data"
                 if (data === "ok") {
                     // le service a été désinstallé
-                    $("#status-radarr").html("Installer").prop('disabled', false);
-                    $(".start-stop-button-radarr").hide();
+                    $("#status-" + appli).html("Installer").prop('disabled', false);
+                    $(".start-stop-button-" + appli).hide();
                 } else {
                     // il y a eu une erreur sur l'installation
-                    $("#status-radarr").html("Erreur sur installation");
-                    $(".start-stop-button-radarr").hide();
+                    $("#status-" + appli).html("Erreur sur installation");
+                    $(".start-stop-button-" + appli).hide();
                 }
             }).fail(function () {
                 console.log('Erreur sur le chargement de l\'ajax, impossible de continuer');
-                $("#status-radarr").html("Erreur ajax");
+                $("#status-" + appli).html("Erreur ajax");
             });
         } else {
             console.log('Erreur sur le texte du bouton, impossible de continuer');
