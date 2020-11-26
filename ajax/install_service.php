2fd759a99d8178e59850ea37622f124e723ba5fd
Merrick <stephane.dewitte@gmail.com>
2020-11-26 17:29:28 +0100

    test

--- ajax/install_service.php
+++ ajax/install_service.php
@@ -3,9 +3,9 @@
 require_once '../php/classes/service.php';
 $service = new service($_GET['service']);
 $subdomain = $_GET['subdomain'];
+echo "Service = " . $_GET['service'] . " et subdomain = " . $subdomain;
 // on lance la fonction install qui va rendre la main presque tout de suite
 // on ne peut faire aucun contrôle dessus
-$service->subdomain = $subdomain;
 $service->install($subdomain);
 echo 'ok';
 
