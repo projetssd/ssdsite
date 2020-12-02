034b487381f052fa7106804b85d1e65caf0640d6
root <laster13@hotmail.com>
2020-12-02 15:51:58 +0100

    update

---
+++ php/classes/rclone.php
@@ -0,0 +1,38 @@
+ <?php
+
+/**
+ * Class client
+ */
+class client
+{
+    /**
+     * @var string Nom du client
+     */
+    public $client;
+    public $secret;
+
+    /**
+     *  constructor.
+     * @param $client id oauth
+     */
+
+    public function __construct($rclone)
+    {
+        $this->client = $rclone;
+
+    }
+
+     */
+    function credential($secret)
+    {
+        $log = new log;
+        $log->writelog("-----------------",'DEBUG');
+        $log->writelog("credential " . $this->client,'DEBUG');
+        /**
+
+        $commande = 'sudo ' . __DIR__ . '/../../scripts/manage_service.sh credential "' . $this->client . '" ';
+        $log->writelog("Lancé","DEBUG");
+        $log->writelog("Commande : " . $commande,"DEBUG");
+        shell_exec($commande);
+    }
+}
