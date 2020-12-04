b7212cd75fe3b04cf3fd78a1a43bc5cf7f04f9ef
Stéphane DEWITTE <stephane.dewitte@gmail.com>
2020-12-04 09:01:16 +0100

    Changement habillage

--- dist/js/changetheme.js
+++ dist/js/changetheme.js
@@ -4,31 +4,20 @@ $(".changetheme").click(function () {
     console.log("Changement de theme");
     var theme = $(this).attr('data-theme');
     console.log("Theme choisi " + theme);
-    switch (theme) {
-        case "adminlte":
-            $('link[href="dist/css/dashboard.min.css"]').prop('disabled', true);
-            $('link[href="dist/css/darkly.min.css"]').prop('disabled', true);
-            $('link[href="dist/css/material.min.css"]').prop('disabled', true);
-            $("#content-global").css("background","#f4f6f9");
-            break;
-        case "darkly":
-            $('link[href="dist/css/dashboard.min.css"]').prop('disabled', true);
-            $('link[href="dist/css/material.min.css"]').prop('disabled', true);
-            //$('link[href="dist/js/adminlte.min.css"]').remove();
-            $("#content-global").css("background","#000000");
-            break;
-        case "material":
-            $('link[href="dist/css/dashboard.min.css"]').prop('disabled', true);
-            $('link[href="dist/css/darkly.min.css"]').prop('disabled', true);
-           // $('link[href="dist/js/adminlte.min.css"]').remove();
-           $("#content-global").css("background","#f4f6f9");
-            break;
-        case "dashboard":
-            $('link[href="dist/css/darkly.min.css"]').prop('disabled', true);
-            $('link[href="dist/css/material.min.css"]').prop('disabled', true);
-            //$('link[href="dist/css/adminlte.min.css"]').remove();
-            $("#content-global").css("background","#f4f6f9");
-            break;
+    $("content-global").css("background", "");
+    $('link[href="dist/css/darkly.min.css"]').prop('disabled', true);
+    $('link[href="dist/css/material.min.css"]').prop('disabled', true);
+    $('link[href="dist/css/circular.min.css"]').prop('disabled', true);
+    $('link[href="dist/css/knightone.min.css"]').prop('disabled', true);
+    /*if(theme === "darkly")
+    {
+        $("#content-global").css("background","#000000");
     }
+    else
+    {
+        $("#content-global").css("background","#f4f6f9");
+    }*/
+
+
     $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', 'dist/css/' + theme + '.min.css'));
 });
