faf7643c5c9078dfb839f239f40484220ad17a15
Merrick <stephane.dewitte@gmail.com>
2020-12-10 14:06:02 +0100

    mises à jour auto ok

--- ajax/maj_git.php
+++ ajax/maj_git.php
@@ -6,10 +6,23 @@ chdir(__DIR__ . '/../');
 exec('/usr/bin/git pull',$return_lines,$return_code);
 if($return_code != 0)
 {
-    return json_encode(array("status" => 0,
+    echo json_encode(array("status" => 0,
                 "message" => $return_lines));
 }
-array_map( 'unlink', array_filter((array) glob(__DIR__ . '/../cache/*') ) );
-array_map( 'unlink', array_filter((array) glob(__DIR__ . '/../dist/js/min/*') ) );
-array_map( 'unlink', array_filter((array) glob(__DIR__ . '/../dist/css/min/*') ) );
-return json_encode(array("status" => 1));
+
+$path_to_delete = array(
+    __DIR__ . '/../cache',
+    __DIR__ . '/../dist/js/min',
+    __DIR__ . '/../dist/css/min'
+    );
+foreach($path_to_delete as $path)
+{
+    $files = glob($path . '/*'); // get all file names
+foreach($files as $file){ // iterate files
+  if(is_file($file)) {
+    unlink($file); // delete file
+  }
+}
+}
+
+echo json_encode(array("status" => 1));
--- scripts/maj_appli.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-DIRNAME=$(dirname $0)
-cd ${DIRNAME}/../
-echo Répertoire courant : $(pwd)
-/usr/bin/git pull
-echo $?
-rm -rf cache/*
-rm -rf dist/js/min/*
-rm -rf dist/css/min/*
