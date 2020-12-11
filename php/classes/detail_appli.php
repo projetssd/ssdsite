a39b6bc9cd6f82c61ff91aa51b3a62d286ce30fc
Merrick <stephane.dewitte@gmail.com>
2020-12-11 14:12:05 +0100

    ajout detail_appli

---
+++ php/classes/detail_appli.php
@@ -0,0 +1,31 @@
+<?php
+
+$tableau_appli = array(
+    // appli bidon
+    "all" => ["port" => 9999],
+    // applis normales
+    "radarr" => ["port" => 7878],
+    'sonarr' => ["port" => 8989],
+    "rutorrent" => ["port" => 8080],
+    "lidarr" => ["port" => 8686],
+    "medusa" => ["port" => 8081],
+    'jackett' =>  ["port" => 9117],
+    'sensorr' =>  ["port" => 443],
+    'emby' =>  ["port" => 8096],
+    'autoscan' =>  ["port" => 3030],
+    'bazarr' =>  ["port" => 6767],
+    'calibre' =>  ["port" => 8080],
+    'calibreweb' =>  ["port" => 8083],
+    'collabora' =>  ["port" => 9980],
+    'db-nextcloud' =>  ["port" => 3306],
+    'coturn' =>  ["port" => 9980],
+    'nextcloud' =>  ["port" => 80],
+    'oauth' =>  ["port" => 80],
+    'office' =>  ["port" => 80],
+    'plex' =>  ["port" => 32400],
+    'portainer' =>  ["port" => 9000],    
+    'tautulli' =>  ["port" => 8181], 
+    'traefik' =>  ["port" => 80], 
+    'ubooquity' =>  ["port" => 2202], 
+    'watchtower' =>  ["port" => 8080], 
+);
