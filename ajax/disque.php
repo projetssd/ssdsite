12b26e4e7e0b2114fad3afa512d6587f4917071f
Merrick <stephane.dewitte@gmail.com>
2021-01-04 15:22:32 +0100

    bugfix espace disque

--- ajax/disque.php
+++ ajax/disque.php
@@ -5,7 +5,8 @@ require_once __DIR__ . '/../includes/header.php';
 $df = disk_free_space('/');
 $dt = disk_total_space('/');
 $du = $dt - $df;
- 
-$dt = ceil($dt/1024/1024/1024);
-$du = ceil($du/1024/1024/1024);
-echo $du.' Go ';
+
+$percent = round(($df/$dt)*100,2); 
+
+$df = ceil($df/1024/1024/1024);
+echo $df.' Go (' . $percent . '%)';
